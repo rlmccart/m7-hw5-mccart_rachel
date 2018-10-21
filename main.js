85399e9d2fb91184a3e5938e5eef861985222f99
Rachel McCart <rlmccart@ufl.edu>
2018-10-20 20:04:19 -0500

    this commit will add content and styles

--- main.js
+++ main.js
@@ -9,11 +9,31 @@ main.appendChild(header);
 var ul = document.createElement('ul');
 main.appendChild(ul);
 
-var li = document.createElement('li');
-ul.appendChild(li);
+var myLi = document.createElement('myLi');
+ul.appendChild(myLi);
 
 var headerText = documnet.createTextNode('This is the header text!');
 header.appendChild(headerText);
 
 var listItemText = document.createTextNode('This is the list item');
-li.appendChild(listItemText);
+myLi.appendChild(listItemText);
+
+header.classList.add('style');
+
+main.classList.add('text-center');
+
+var link = document.createElement('a');
+
+var linkListItem = document.createElement('li'); 
+
+link.appendChild(linkListItem);
+linkListItem.appendChild(ul);
+
+var linkText = document.createTextNode('This is my link');
+linkText.appendChild(link);
+
+link.href = 'https://www.google.com';
+
+var notLinkListItem = linkListItem.previousElementSibling;
+
+notLinkListItem.classList.add('orange');
