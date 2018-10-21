42dfd18cd50b92a64a5f9ad3eb33c7bca0216dba
Rachel McCart <rlmccart@ufl.edu>
2018-10-20 19:32:30 -0500

    this commit will create variables and add elements to main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var main = document.createElement('main');
+var pageBody = document.getElementsByTagName('body')[0];
+var scriptTag = document.getElementsByTagName('script')[0];
+pageBody.insertBefore(main, scriptTag);
+
+var header = document.createElement('h1');
+main.appendChild(header);
+
+var ul = document.createElement('ul');
+main.appendChild(ul);
+
+var li = document.createElement('li');
+ul.appendChild(li);
